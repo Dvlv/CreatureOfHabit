91deaf3c0ba12526855828428638a52e5eb565c9
Dvlv <Dvlv@users.noreply.github.com>
2023-07-08 15:10:31 +0100

    create-read-update-delete of habits

--- creature-of-habit.js
+++ creature-of-habit.js
@@ -8,6 +8,40 @@ function deserializeHabitsFromStorage() {
     return habits ? JSON.parse(habits) : {};
 }
 
+function deserializeDaysHabitsFromStorage() {
+    var daysHabits = localStorage.getItem('daysHabits');
+    var habits = deserializeHabitsFromStorage();
+
+    if (!daysHabits) {
+        localStorage.setItem('daysHabits', JSON.stringify({}));
+        daysHabits = {}
+    }
+    if (!habits) {
+        localStorage.setItem('habits', JSON.stringify({}));
+        habits = {};
+    }
+
+    daysHabits = JSON.parse(daysHabits);
+
+    var deserialized = {}
+
+    for (var day in daysHabits) {
+        var ids = daysHabits[day];
+        for (var i = 0; i < ids.length; i++) {
+            var id = ids[i];
+            if (habits[id]) {
+                if (deserialized[day]) {
+                    deserialized[day].push({id: habits[id].id, action: habits[id].action});
+                } else {
+                    deserialized[day] = [{id: habits[id].id, action: habits[id].action}];
+                }
+            }
+        }
+    }
+
+    return deserialized;
+}
+
 function getTodaysHabits() {
     var today = new Date();
 }
@@ -47,48 +81,116 @@ function getTodaysDate() {
 }
 
 function addHabit(fd) {
-    var allHabits = deserializeHabitsFromStorage();
     const action = fd.get('action');
 
+    var nextHabitId = 0;
+    if (fd.get("id")) {
+        nextHabitId = fd.get("id");
+    } else {
+        nextHabitId = localStorage.getItem('nextHabitId') || 0;
+    }
+
+    var newHabit = {
+        id: nextHabitId,
+        action: action,
+        frequency: []
+    }
+
     daysOfWeek.forEach(day => {
-        if (fd.get(day) == '1') {
-            var daysHabits = allHabits[day];
-            if (!daysHabits) {
-                allHabits[day] = [];
-            }
-            allHabits[day].push(action);
+        if (fd.get(day)) {
+            newHabit.frequency.push(day);
         }
     })
 
-    localStorage.setItem('habits', JSON.stringify(allHabits));
+    var habits = deserializeHabitsFromStorage();
+    habits[newHabit.id] = newHabit;
+
+    localStorage.setItem('habits', JSON.stringify(habits));
+    localStorage.setItem('nextHabitId', parseInt(nextHabitId) + 1);
+
+    buildDaysHabits()
+}
+
+function buildDaysHabits() {
+    var daysHabits = {
+        "Mon": [],
+        "Tues": [],
+        "Wed": [],
+        "Thurs": [],
+        "Fri": [],
+        "Sat": [],
+        "Sun": []
+    };
+
+    var habits = deserializeHabitsFromStorage();
+
+    for (var habitId in habits) {
+        var habit = habits[habitId];
+        var freq = habit.frequency;
+        for (var i = 0; i < freq.length; i++) {
+            var day = freq[i];
+            daysHabits[day].push(habit.id);
+        }
+    }
+
+    localStorage.setItem('daysHabits', JSON.stringify(daysHabits));
 }
 
 function showHabitList() {
-    var habits = deserializeHabitsFromStorage();
-    daysOfWeek.forEach(day => {
-        var daysHabits = habits[day];
-        if (daysHabits) {
-            var habitList = document.querySelector('#habit-list #' + day + ' ul');
-            habitList.innerHTML = '';
-            daysHabits.forEach(habit => {
-                var li = document.createElement('li');
-                li.innerText = habit;
+    var daysHabits = deserializeDaysHabitsFromStorage();
 
-                var editBtn = document.createElement('button');
-                editBtn.innerText = 'E';
-                li.appendChild(editBtn);
+    daysOfWeek.forEach(day => {
+        var habitDay = daysHabits[day];
+        var habitList = document.querySelector('#habit-list #' + day + ' ul');
+        habitList.innerHTML = '';
+
+        if (habitDay) {
+            for (var habit of habitDay) {
+                var li = document.createElement('li');
+                var sp = document.createElement('span');
+
+                sp.innerText = habit.action;
+                sp.setAttribute('onclick', 'editHabit(' + habit.id + ')');
+                li.appendChild(sp);
 
                 var delBtn = document.createElement('button');
                 delBtn.innerText = 'X';
+                delBtn.setAttribute('onclick', 'removeHabit(' + habit.id + ', "' + day + '")');
                 li.appendChild(delBtn);
 
-
                 habitList.appendChild(li);
-            })
+            }
         }
     })
 
     document.getElementById('habit-list').style.display = 'flex';
+}
+
+function hideHabitList() {
+    document.getElementById('habit-list').style.display = 'none';
+}
+
+function editHabit(habitId) {
+    var habits = deserializeHabitsFromStorage();
+    var habit = habits[habitId];
+
+    document.getElementById('id').value = habitId;
+    document.getElementById('action').value = habit.action;
+    daysOfWeek.forEach(day => {
+        document.getElementById(day).checked = habit.frequency.includes(day);
+    });
+
+    hideHabitList();
+    showHabitForm();
+}
+
+function removeHabit(habitId, day) {
+    var habits = deserializeHabitsFromStorage();
+    delete habits[habitId];
+    localStorage.setItem('habits', JSON.stringify(habits));
+    buildDaysHabits();
+    hideHabitList();
+    showHabitList();
 }
 
 function updateDateTime() {
@@ -125,6 +227,7 @@ function updateStreak() {
 }
 
 updateDateTime();
+setInterval(updateDateTime, 60000);
 updateCoins();
 updateStreak();
 
